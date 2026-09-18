f3d6cfc6b93910c5437da575d198f2d2ffb92a9b
Matthew Cho <matthewcho05@gmail.com>
2026-09-18 14:55:17 -0400

    chore(infrastructure): configure Python CDK environment

---
+++ infrastructure/stacks/auth_stack.py
@@ -0,0 +1,17 @@
+import aws_cdk as cdk
+from constructs import Construct
+
+class AuthStack(cdk.Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            scope,
+            construct_id,
+            **kwargs,
+        )
+
+        # Cognito infrastructure will be implemented here.
